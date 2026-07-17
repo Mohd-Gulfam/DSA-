772201fe26de5cc326e440a921f66c551ade1355
Mohd Gulfam <mg5708445@gmail.com>
2026-07-17 11:57:45 +0530

    Time: 22 ms (96.12%), Space: 48.9 MB (33.97%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,17 +1,43 @@
+import java.util.HashMap;
+
 class Solution {
-    public int subarraySum(int[] nums, int k) {
-        int n = nums.length;
+    public int subarraySum(int[] arr, int k) {
+
+        int n = arr.length;
         int count = 0;
-        for(int i = 0; i<n ; i++){
-            int sum = 0;
-            for(int j=i; j<n; j++){
-                sum +=nums[j];
-                if(sum==k){
-                    count++;
-                }
+
+        int[] prefix = new int[n];
+        prefix[0] = arr[0];
+
+        // Build prefix sum array
+        for (int i = 1; i < n; i++) {
+            prefix[i] = prefix[i - 1] + arr[i];
+        }
+
+        HashMap<Integer, Integer> hm = new HashMap<>();
+
+        for (int j = 0; j < n; j++) {
+
+            // Case 1: Subarray starts from index 0
+            if (prefix[j] == k) {
+                count++;
+            }
+
+            // Case 2: Check if (prefix[j] - k) exists
+            int val = prefix[j] - k;
+
+            if (hm.containsKey(val)) {
+                count += hm.get(val);
+            }
+
+            // Store current prefix sum
+            if (hm.containsKey(prefix[j])) {
+               hm.put(prefix[j], hm.get(prefix[j]) + 1);
+            } else {
+               hm.put(prefix[j], 1);
             }
         }
+
         return count;
-        
     }
 }
