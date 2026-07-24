1bc72fe203108983369172d8b5f8626f5b9e7089
Mohd Gulfam <mg5708445@gmail.com>
2026-07-24 12:55:04 +0530

    Time: 7 ms (56.36%), Space: 47.5 MB (8.46%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,25 +1,23 @@
 class Solution {
     public boolean canFinish(int n, int[][] pre) {
         List<List<Integer>> adj = new ArrayList<>();
-        for(int i=1; i<=n; i++){
+        for(int i = 0; i<= n ; i++){
             adj.add(new ArrayList<>());
-
         }
         int[] indegree = new int[n];
-        boolean[] vis = new boolean[n];
-
-        for(int i = 0 ; i<pre.length; i++){
-            int a = pre[i][0], b = pre[i][1]; //edge u-->v
+        boolean[] visited = new boolean[n];
+        for(int i =0; i<pre.length; i++){
+            int a = pre[i][0], b = pre[i][1];
             adj.get(b).add(a);
-            indegree[a]++; 
+            indegree[a]++;
         }
-        // kahn's algorithm
+        // kahns algo
         Queue<Integer> q = new LinkedList<>();
         List<Integer> ans = new ArrayList<>();
-        for(int i =0; i<n; i++){
+        for(int i= 0; i<n; i++){
             if(indegree[i]==0){
                 q.add(i);
-                vis[i]=true;
+                visited[i] = true;
             }
         }
         while(q.size()>0){
@@ -27,16 +25,14 @@ class Solution {
             ans.add(front);
             for(int ele: adj.get(front)){
                 indegree[ele]--;
-                if(indegree[ele]==0){
+                if(indegree[ele] == 0){
                     q.add(ele);
-                    vis[ele]= true;
-
+                    visited[ele] = true;
                 }
             }
         }
-        if(ans.size()==n) return true;
+        if(ans.size()== n) return true;
         else return false;
-
         
     }
 }
