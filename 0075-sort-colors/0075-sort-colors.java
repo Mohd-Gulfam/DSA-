b9298517b161e531925b88323be9682e991eef0f
Mohd Gulfam <mg5708445@gmail.com>
2026-09-08 11:02:36 +0530

    Time: 0 ms (100%), Space: 43.4 MB (76.38%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,16 +1,40 @@
 class Solution {
-    public void sortColors(int[] nums) {
-        int[] freq = new int[3];
+    public void sortColors(int[] arr) {
+        // int[] freq = new int[3];
 
-        for (int i = 0; i < nums.length; i++) {
-            freq[nums[i]]++;
-        }
-        int k = 0;
-        for (int i = 0; i < freq.length; i++) {
-            while (freq[i]-- > 0) {
-                nums[k++] = i;
+        // for (int i = 0; i < nums.length; i++) {
+        //     freq[nums[i]]++;
+        // }
+        // int k = 0;
+        // for (int i = 0; i < freq.length; i++) {
+        //     while (freq[i]-- > 0) {
+        //         nums[k++] = i;
+        //     }
+
+        // }
+        int low = 0;
+        int mid = 0;
+        int high = arr.length - 1;
+
+        while (mid <= high) {
+
+            if (arr[mid] == 0) {
+                swap(arr, low, mid);
+                low++;
+                mid++;
             }
-
+            else if (arr[mid] == 1) {
+                mid++;
+            }
+            else {
+                swap(arr, mid, high);
+                high--;
+            }
         }
+    }
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
